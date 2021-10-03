ba5e6576a1d4d8a0333f7bfc5203fb1e2ad8a835
FrogasQ <91828204+FrogasQ@users.noreply.github.com>
2021-10-03 18:51:50 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const mineflayer = require("mineflayer");
+const bot = mineflayer.createBot({ 
+	host: "hivermcnetwork.aternos.me",
+	port: 63409,
+    username: "FrogasBot" 
+});
+
+function lookAtNearestPlayer(){
+    const playerFilter = (entity) => entity.type === "player";
+    const playerEntity = bot.nearestEntity(playerFilter);
+    if(!playerEntity) return;
+    const pos = playerEntity.position.offset(0, playerEntity.height, 0);
+    bot.lookAt(pos);
+}
+
+bot.on("physicTick", lookAtNearestPlayer);
